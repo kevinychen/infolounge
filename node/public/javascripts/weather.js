650f5fd04204dd18d270da9068acd11e69103159
kyc <kyc2915@mit.edu>
2013-04-25 21:44:23 -0400

    Weather: black text and prettier pictures.

--- node/public/javascripts/weather.js
+++ node/public/javascripts/weather.js
@@ -4,10 +4,10 @@ function getWeather() {
         unit: 'f',
         success: function(weather) {
             html = '<table>';
-            html += '<tr><td><img width="135px" src="images/weather/'+weather.code+'.png"></td>';
+            html += '<tr><td><img width="135px" src="images/weather/'+weather.code+'d.png"></td>';
             html += '<td><span class="small">temp | feel</span><br/><span class="big_bold">'+weather.temp+'&deg;'+weather.units.temp+' | ' + weather.wind.chill + '&deg;' + weather.units.temp + '</span><br/><br/><span class="medium">' + weather.currently + '</span></td><td></td>';
 
-            html += '<td><img width="135px" src="images/weather/' + weather.todayCode + '.png">';
+            html += '<td><img width="135px" src="images/weather/' + weather.todayCode + 'd.png">';
             html += '<td><span class="medium">Today</span><br/><br/><span class="big">' + weather.high + '&deg;' + weather.units.temp + ' | ' + weather.low + '&deg;' + weather.units.temp + '</span><br/><br/><span class="medium">' + weather.forecast + '</span></td></tr>';
             html += '</table>';
 
