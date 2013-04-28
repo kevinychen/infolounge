e9a19e3e024964e298359df59ac4fb0499aad24f
poofytoo <victorhung92@gmail.com>
2013-04-28 16:50:43 -0400

    birds

--- node/public/javascripts/date.js
+++ node/public/javascripts/date.js
@@ -1,9 +1,39 @@
 
 function getDate() {
     var now = new Date();
-    var elem = dateFormat(now, 'ddd, mmm d');
-    var becky = new Date("September 4, 1993 00:00:00");
-    elem += '<div class="subdate">becky is ' + ((now - becky) / 1000 / 60 / 60 / 24 / 365.25).toFixed(8) + ' years old</div>';
+
+    var people = [
+        ['kyc', 'September 3, 1992 21:05:00'],
+        ['khu', 'November 21, 1992 08:00:00'],
+        ['natalle', 'December 3, 1992 00:00:00'],
+        ['eva', 'February 13, 1993 19:30:00'],
+        ['shanthi', 'June 12, 1993 18:12:00'],
+        ['michaelx', 'July 28, 1993 00:00:00'],
+        ['becky', 'September 4, 1993 18:00:00'],
+        ['sumit', 'December 19, 1993 00:00:00'],
+        ['norman', 'February 5, 1993 06:00:00'],
+    ];
+
+    var dict = [];
+    for (var i = 0; i < people.length; i += 1) {
+	var age = 0;
+	var date = new Date(people[i][1]);
+	while (date < now) {
+	    age++;
+	    date.setYear(date.getFullYear() + 1);
+	}
+	age--;
+	date.setYear(date.getFullYear() - 1);
+	age += (now - date) / 1000 / 60 / 60 / 24 / (now.getYear() % 4 == 0 ? 366 : 365);
+	var ageStr = age.toFixed(8);
+	var fracpart = ageStr.substring(ageStr.indexOf('.'));
+	dict.push([fracpart, '<div class="subdate">' + people[i][0] + ' is ' + ageStr + ' years old</div>']);
+    }
+    dict.sort();
+    var elem = dateFormat(now, 'ddd, mmmm d');
+    for (var i = dict.length; --i >= dict.length - 5; ) {
+	elem += dict[i][1];
+    }
     $("#date").html(elem);
 }
 
