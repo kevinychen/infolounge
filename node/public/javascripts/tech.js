96e6d057dd808814fe02de56775c306059be94f0
tim <tim@mplcr.mit.edu>
2014-02-04 22:24:50 -0500

    fuck, this is the right version

--- node/public/javascripts/tech.js
+++ node/public/javascripts/tech.js
@@ -1,47 +1,109 @@
-var stopId = 51; // Next House stop ID
+var predictionsTEK = "http://proximobus.appspot.com/agencies/mit/stops/51/predictions.json";
+	// tech shuttle and saferide cambridge, at tang/westgate
 
-var predictionsURL = "http://proximobus.appspot.com/agencies/mit/stops/" + stopId + "/predictions.json";
+var predictionsSFBOS = "http://proximobus.appspot.com/agencies/mit/stops/62/predictions.json";
+	// saferide boston, at audrey/vassar sts
 
-function handlePredictions(data) {
+var predictionsCT2N = "http://proximobus.appspot.com/agencies/mbta/stops/21772/predictions.json";
+	// crosstown 2, to sullivan sq via kendall sq, at amesbury/vassar sts
+
+var predictionsCT2S = "http://proximobus.appspot.com/agencies/mbta/stops/22173/predictions.json";
+	// crosstown 2, to ruggles st/northereastern university via fenway, at amesbury st/memorial dr
+
+var elem = '';
+var tone = 0;
+
+function handlePredictions2(data) {
     var predictions = data.items;
 
     if (predictions.length == 0) {
-        $("#techpanel").slideUp("slow");
+        elem = '';
         return;
     }
 
-    var elem = '';
     for (var i = 0; i < 2; i++) {
         prediction = predictions[i];
-        elem += '<li><span class="tech-minutes">';
+
+	if (i == 0) {
+            elem = '<li><span class="tech-minutes2">';
+	} else {
+	    elem += '<li><span class="tech-minutes2">';}
 
         var minutes = prediction.minutes;
         if (minutes == 0) {
-            elem += (prediction.is_departing ? "Departing" : "Arriving");
+            elem += (prediction.is_departing ? "Arrv</span>&nbsp;<span class='tech-route2'>" : "Arrv</span>&nbsp;<span class='tech-route2'>");
         } else {
-            elem += (minutes + " min");
+            elem += (minutes + '</span>&nbsp;<span class="tech-route2">m ');
         }
 
         var route = '';
         if (prediction.route_id == 'tech') {
-            route = 'Tech to Kendall Sq';
+            route = 'Tech Shuttle';
         } else if (prediction.route_id == 'saferidecambwest') {
-            route = 'Saferide to Central Sq';
+            route = 'Saferide Cambridge';
         } else if (prediction.route_id == 'saferidecamball') {
-            route = 'Saferide to Central Sq';
+            route = 'Saferide Cambridge';
         } else if (prediction.route_id == 'traderjwf') {
-            route = 'Saferide to Trader Joes/Central Sq';
+            route = 'Central/Kendall Sqs';
         } else {
             route = prediction.route_id;
         }
-        elem += ('</span>&nbsp;&nbsp;<span class="tech-route"><br/>' + route + '</span></li>');
+        elem += ('&nbsp;-&nbsp;&nbsp;' + route + '</span></li>');
     }
 
     $("#techpanel").slideDown("slow");
     $("#predictions").html(elem);
 }
 
+function handlePredictions(data) {
+    var predictions = data.items;
+
+    for (var i = 0; i < 1; i++) {
+        prediction = predictions[i];
+        elem += '<li><span class="tech-minutes">';
+
+        var minutes = prediction.minutes;
+        if (minutes == 0) {
+            elem += (prediction.is_departing ? "Arrv</span>&nbsp;<span class='tech-route'>" : "Arrv</span>&nbsp;<span class='tech-route'>");
+        } else {
+            elem += (minutes + '</span>&nbsp;<span class="tech-route">m ');
+        }
+
+        var route = '';
+        if (prediction.route_id == 'saferidebostonall') {
+            route = 'ZBT/Packard\'s Corner';
+        } else if (prediction.route_id == 'saferidebostonw') {
+            route = 'ZBT/Packard\'s Corner';
+        } else if (prediction.route_id == '747' && tone == 0) {
+            route = 'CT2, Fenway/Northeastern';
+	    tone = 1;
+        } else if (prediction.route_id == '747' && tone == 1) {
+            route = 'CT2, Kendall/Sullivan Sqs';
+        } else {
+            route = prediction.route_id;
+        }
+        elem += ('&nbsp;-&nbsp;&nbsp;' + route + '</span></li>');
+    }
+
+    $("#techpanel").slideDown("slow");
+    $("#predictions").html(elem);
+}
+
+
 function getPredictions() {
-    $.getJSON(predictionsURL, handlePredictions);
+    $.getJSON(predictionsTEK, handlePredictions2);
+    $.getJSON(predictionsSFBOS, handlePredictions);
+    $.getJSON(predictionsCT2N, handlePredictions);
+    $.getJSON(predictionsCT2S, handlePredictions);
 };
 
+console.log('Elem content:');
+console.log(elem);
+console.log('Elem length:');
+console.log(''+elem.length);
+
+if (elem.length == 0) {
+	console.log('ONE');
+    $("#techpanel").slideUp("slow");
+	console.log('TWO');
+}
